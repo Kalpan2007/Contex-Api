c114a5dc28e73d167e7d9e920a7e48cbe71d31b1
Kalpan2007 <kalpankaneriya@gmail.com>
2024-12-17 11:11:06 +0530

    complte the project of Toggle by contex

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import { useState } from 'react'
-import UserContaxProvider from './Contex/UserContexProv'
-import Login from './Component/login';
-import Profile from './Component/profile';
-
-
-
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "./Contex/Theame";
+import Btn from "./Components/Btn";
+import Card from "./Components/Card";
 
 function App() {
+  const [themeMode,setThemeMode]=useState('light')
+  const lightTheme=()=>{
+    setThemeMode('light')
+  }
+  const darkTheme=()=>{
+    setThemeMode('dark')
+  }
+
+  useEffect(() => {
+    const htmlElement = document.querySelector('html');
+    htmlElement.classList.remove('light', 'dark');
+    if (themeMode) {
+        htmlElement.classList.add(themeMode);
+    }
+}, [themeMode]);
 
   return (
-    <UserContaxProvider>
-     <Login/>
-     <Profile/>
-    </UserContaxProvider>
+    <>
+    <ThemeProvider value={{themeMode,darkTheme,lightTheme}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Btn/>
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+      </ThemeProvider>
+
+    </>
   )
 }
 
--- src/Contex/UserContax.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Usercontex = React.createContext()
-
-
-export default Usercontex;
-
